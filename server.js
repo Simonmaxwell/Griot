c66ca843843629e6dd522c5efd6ddd4ba8205206
The Good Maxwell <maxwellraden@gmail.com>
2017-08-27 08:16:27 -0500

    initial set up

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const router = require('./router');
+
+app.use(morgan('common'));
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
