095d9b2492245f5116c115b8e681e4eb77e45a75
The Good Maxwell <maxwellraden@gmail.com>
2017-10-01 11:28:07 -0500

    complete set of CRUD operations

--- router.js
+++ router.js
@@ -29,6 +29,32 @@ router.post('/character-sheet', jsonParser, (req, res) => {
     res.status(201).json(item);
 });
 
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = ['id', 'name', 'level'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`
+  		console.error(message);
+  		return res.status(400).send(message);
+		}
+	}
+	if (req.params.id !== req.body.id) {
+    const message = (
+    	`Your dang id: (${req.params.id}) doesn't match this id: `
+    	`(${req.body.id}) ya doofus!`);
+    console.error(message);
+   	return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+  	id: req.params.id,
+  	name: req.body.name,
+  	level: req.body.level
+  });
+  res.status(200).json(updatedItem);
+});
+
 
 router.delete('/character-sheet/:id', function(req, res) {
 });
