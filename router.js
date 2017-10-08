076bb60a101c22ae0c74fd634b412aaa518bf511
The Good Maxwell <maxwellraden@gmail.com>
2017-10-08 12:51:28 -0500

    completed character sheet models

--- router.js
+++ router.js
@@ -15,6 +15,10 @@ router.get('/character-sheet', (req, res) => {
 	res.json(CharacterSheet.get());
 });
 
+router.get('/character-sheet/:id', (req, res) => {
+	res.json(CharacterSheet.get(req.params.id));
+});
+
 router.post('/character-sheet', jsonParser, (req, res) => {
 	const requiredFields = ['name', 'level'];
 	for (let i=0; i<requiredFields.length; i++) {
@@ -22,32 +26,32 @@ router.post('/character-sheet', jsonParser, (req, res) => {
 		if (!(field in req.body)) {
   		const message = `Missing \`${field}\` in request body`
   		console.error(message);
-  		return res.status(400).send(message);
+  		res.status(400).send(message);
 		}
 	}
 	const item = CharacterSheet.create(req.body.name, req.body.level);
     res.status(201).json(item);
 });
 
-router.put('/:id', jsonParser, (req, res) => {
-	const requiredFields = ['id', 'name', 'level'];
+router.put('/character-sheet/:id', jsonParser, (req, res) => {
+	const requiredFields = [ 'id', 'name', 'level'];
 	for (let i=0; i<requiredFields.length; i++) {
 		const field = requiredFields[i];
 		if (!(field in req.body)) {
 			const message = `Missing \`${field}\` in request body`
   		console.error(message);
-  		return res.status(400).send(message);
+  		res.status(400).send(message);
 		}
 	}
 	if (req.params.id !== req.body.id) {
-    const message = (
-    	`Your dang id: (${req.params.id}) doesn't match this id: `
-    	`(${req.body.id}) ya doofus!`);
-    console.error(message);
-   	return res.status(400).send(message);
-  }
+     const message = (
+     	`Your dang id: (${req.params.id}) doesn't match this id: `
+     	`(${req.body.id}) ya doofus!`);
+     console.error(message);
+    	res.status(400).send(message);
+   }
   console.log(`Updating blog post with id \`${req.params.id}\``);
-  const updatedItem = BlogPosts.update({
+  const updatedItem = CharacterSheet.update({
   	id: req.params.id,
   	name: req.body.name,
   	level: req.body.level
@@ -56,7 +60,9 @@ router.put('/:id', jsonParser, (req, res) => {
 });
 
 
-router.delete('/character-sheet/:id', function(req, res) {
+router.delete('/character-sheet/:id', (req, res) => {
+	CharacterSheet.delete(req.params.id);
+	res.status(204).send();
 });
 
 module.exports = router;
