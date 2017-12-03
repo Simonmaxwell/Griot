bac7733809e9f4348506b12aeaa404333d6f10c2
The Good Maxwell <maxwellraden@gmail.com>
2017-12-03 14:11:40 -0600

    fixed empty body object bug. POST is working

--- JQfront/app.js
+++ JQfront/app.js
@@ -12,17 +12,33 @@ $(document).ready(function() {
 
 	function showCharacters(data) {
 		console.log(data);
+		let options = "";
+		for(let i =0; i < data.length; i++) {
+			options += `<option value="${data[i]._id}"> ${data[i].name}</option>`
+		}
+		$("#character-dropdown").html(options);
 	};
 
 	$("#character-form").submit(function(e) {
 		console.log("what");
 		e.preventDefault();
 		let character = {};
-		character.name = $("#name").val();
-		character.level = parseInt($("#level").val());
+		character.name = $("#new-name").val();
+		character.level = parseInt($("#new-level").val());
 		console.log(character);
 		postCharacter(character);
 	});
+
+	$("#update-character-form").submit(function(e) {
+		e.preventDefault();
+		let updatedCharacter = {
+			_id : $("#character-dropdown").find(":selected").val(),
+		};
+		updatedCharacter.name = $("#name").val();
+		updatedCharacter.level = $("#level").val();
+		console.log(updatedCharacter);
+		putCharacter(updatedCharacter);
+	})
 
 	function postCharacter(data) {
 		let settings = {
@@ -34,6 +50,18 @@ $(document).ready(function() {
 			success: showCharacters
 		};
 		console.log(settings);
+		$.ajax(settings);
+	}
+
+	function putCharacter(data) {
+		let settings = {
+			url: 'http://localhost:8090/character-sheet/' + data._id,
+			type: 'PUT',
+			dataType: 'json',
+			contentType: 'application/json',
+			data: JSON.stringify(data),
+			success: showCharacters
+		};
 		$.ajax(settings);
 	}
 
