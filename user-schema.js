95b9e19baefba8d9f92057b3b6898a34dc4c27c1
The Good Maxwell <maxwellraden@gmail.com>
2018-02-25 09:30:38 -0600

    added user-schema

---
+++ user-schema.js
@@ -0,0 +1,34 @@
+//Mongoose pres-save password hashing from: http://devsmash.com/blog/password-authentication-with-mongoose-and-bcrypt
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+    username: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true }
+});
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+const mongoose.model('User', UserSchema);
+
+module.exports = {UserSchema};
