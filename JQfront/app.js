d81ce197190f1440a44d9e95fa24bb308b848adc
The Good Maxwell <maxwellraden@gmail.com>
2018-03-18 10:46:11 -0500

    edit button supplies _id to update-character-form, code cleanup of app.js

--- JQfront/app.js
+++ JQfront/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-	$(".character-sheet-container").hide();
-	
 	function getCharacters() {
 		let authToken = localStorage.getItem("token");
 		let settings = {
@@ -11,88 +9,10 @@ $(document).ready(function() {
 			},
 			type: 'GET',
 			dataType: 'json',
-			success: showCharacters
+			success: characterDisplay
 		};
 		$.ajax(settings);
-
 	};
-
-	function showCharacters(data) {
-		console.log(data);
-		let options = "";
-		for(let i =0; i < data.length; i++) {
-			options += `<option value="${data[i]._id}"> ${data[i].name}</option>`
-		}
-		$("#character-dropdown").html(options);
-		characterDisplay(data);
-	};
-
-	function renderCharacter(character) {
-		var html = `
-		<div class="rendered-character" data=${character._id}>
-			<button class="edit">edit</button>
-			<button class="delete">delete</button>
-			<h3 class="character-name">${character.name}</h3>
-			<h4 class="character-level">${character.level}</h4>
-			<div class="stats">
-				<p>influence:${character.influence}</p>
-				<p>presence:${character.presence}</p>
-				<p>sympathy:${character.sympathy}</p>
-				<p>resolve:${character.resolve}</p>
-				<p>elegance:${character.elegance}</p>
-			</div>
-		</div>`
-
-		return html;
-	};
-
-	$(document).on("click",".edit", function() {
-		console.log($(this).parent().attr("data"));
-	});
-
-	$(document).on("click",".delete", function() {
-		deleteCharacter($(this).parent().attr("data"));
-	});
-
-	function characterDisplay(data) {
-		var html = '';
-		for(let i = 0; i <data.length; i++) {	
-			html += 
-			renderCharacter(data[i]);
-		}
-		$("#test-div").html(html);
-	};
-
-	$("#character-form").submit(function(e) {
-		console.log("what");
-		e.preventDefault();
-		let character = {};
-		character.name = $("#new-name").val();
-		character.species = $("#new-species").val();
-		character.level = parseInt($("#new-level").val());
-		character.influence = parseInt($("#new-influence").val());
-		character.presence = parseInt($("#new-presence").val());
-		character.sympathy = parseInt($("#new-sympathy").val());
-		character.resolve = parseInt($("#new-resolve").val());
-		character.elegance = parseInt($("#new-elegance").val());
-		console.log(character);
-		postCharacter(character);
-	});
-
-	$("#update-character-form").submit(function(e) {
-		e.preventDefault();
-		let updatedCharacter = {
-			_id : $("#character-dropdown").find(":selected").val(),
-		};
-		updatedCharacter.level = parseInt($("#level").val());
-		updatedCharacter.influence = parseInt($("#influence").val());
-		updatedCharacter.presence = parseInt($("#presence").val());
-		updatedCharacter.sympathy = parseInt($("#sympathy").val());
-		updatedCharacter.resolve = parseInt($("#resolve").val());
-		updatedCharacter.elegance = parseInt($("#elegance").val());
-		console.log(updatedCharacter);
-		putCharacter(updatedCharacter);
-	})
 
 	function postCharacter(data) {
 		let settings = {
@@ -129,6 +49,79 @@ $(document).ready(function() {
 		};
 		$.ajax(settings);
 	}
+
+	function characterRender(character) {
+		var html = `
+		<div class="rendered-character" data=${character._id}>
+			<button class="edit">edit</button>
+			<button class="delete">delete</button>
+			<h3 class="character-name">${character.name}</h3>
+			<h4 class="character-level">Level:${character.level}</h4>
+			<div class="stats">
+				<p>influence:${character.influence}</p>
+				<p>presence:${character.presence}</p>
+				<p>sympathy:${character.sympathy}</p>
+				<p>resolve:${character.resolve}</p>
+				<p>elegance:${character.elegance}</p>
+			</div>
+		</div>`
+		return html;
+	};
+
+	function characterDisplay(data) {
+		var html = '';
+		for(let i = 0; i <data.length; i++) {	
+			html += 
+			characterRender(data[i]);
+		}
+		$("#test-div").html(html);
+	};
+
+	$(".character-sheet-container").hide();
+	$("#update-character").hide()
+
+
+	$(document).on("click",".edit", function() {
+		$("#update-character").show();
+		let selectedCharacter = $(this).parent().attr("data");
+		$("#update-character").attr("data-woohoo", selectedCharacter);
+	});
+
+	$(document).on("click",".delete", function() {
+		deleteCharacter($(this).parent().attr("data"));
+	});
+
+	$("#character-form").submit(function(e) {
+		console.log("what");
+		e.preventDefault();
+		let character = {};
+		character.name = $("#new-name").val();
+		character.species = $("#new-species").val();
+		character.level = parseInt($("#new-level").val());
+		character.influence = parseInt($("#new-influence").val());
+		character.presence = parseInt($("#new-presence").val());
+		character.sympathy = parseInt($("#new-sympathy").val());
+		character.resolve = parseInt($("#new-resolve").val());
+		character.elegance = parseInt($("#new-elegance").val());
+		console.log(character);
+		postCharacter(character);
+	});
+
+	$("#update-character-form").submit(function(e) {
+		e.preventDefault();
+		let updatedCharacter = {
+			_id : $("#update-character").attr("data-woohoo"),
+		};
+		updatedCharacter.level = parseInt($("#level").val());
+		updatedCharacter.influence = parseInt($("#influence").val());
+		updatedCharacter.presence = parseInt($("#presence").val());
+		updatedCharacter.sympathy = parseInt($("#sympathy").val());
+		updatedCharacter.resolve = parseInt($("#resolve").val());
+		updatedCharacter.elegance = parseInt($("#elegance").val());
+		console.log(updatedCharacter);
+		putCharacter(updatedCharacter);
+		$("#update-character").hide();
+	})
 
 	getCharacters();
 
